cd5b5118406cd51794cc183f56333b7b68611f8c
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-17 13:58:37 +0100

    F hecho

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ProgressBar } from 'react-bootstrap';
 
 export function Perfil() {
   return (
-    <div className="main-site">
-      <h1>Página de perfil</h1>
-    </div>
+    <div>
+  <ProgressBar animated now striped variant="success" now={40} />
+  <ProgressBar animated now striped variant="info" now={20} />
+  <ProgressBar animated now striped variant="warning" now={60} />
+  <ProgressBar animated now striped variant="danger" now={80} />
+</div>
   );
 }
