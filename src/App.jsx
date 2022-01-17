b26e4d8ceabfc728596be50aa3f06a054f14d798
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-17 13:17:47 +0100

    Bootstrap cambio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Home } from './components/Home';
-import { PaginaListas } from './components/PaginaListas';
-import { Perfil } from './components/Perfil';
 import Header from './components/Header';
 import BootstrapHeader from './components/BootstrapHeader';
 import { MenuItems } from './data/MenuItems';
