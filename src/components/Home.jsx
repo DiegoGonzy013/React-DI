a2096325cdbd14151599f23dc0f73446c5b2cbba
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-18 10:53:54 +0100

    Intentado tabla

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,12 @@
 import React from 'react';
 
-import { Table } from 'react-bootstrap';
-
+import  BootstrapTabla from './BootstrapTabla';
 
 export function Home() {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+    <div className="main-state">
+      <h1>Pedazo tabla mirmano</h1>
+    <BootstrapTabla/>
+    </div>
   );
 }
---
+++ src/data/TablaItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    id: 1,
+    title: 'Home',
+    component: Home,
+  },
+  {
+    id: 2,
+    title: 'Listas de tareas',
+    component: PaginaListas,
+  },
+  {
+    id: 3,
+    title: 'Perfil',
+    component: Perfil,
+  },
+];
