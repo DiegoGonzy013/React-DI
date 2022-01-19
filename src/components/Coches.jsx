fc34438e2bec8a621d80dff1f532e2feb716677e
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-19 17:09:21 +0100

    Entrega tabla dinamica, intentado pero no doy con el punto

--- src/components/Coches.jsx
+++ src/components/Coches.jsx
@@ -7,12 +7,27 @@ class Coches extends React.Component {
     super(props);
     this.state = {
       clicked: false,
+      //listaF1: this.changeData(props.DatosCircuitos)
     };
   }
-
-  changeClicked() {
+  changeData() {
     this.setState({ clicked: this.state.clicked });
   }
+  /*changeData(DatosCircuitos) {
+    const lista = [];
+    this.setState({ clicked: this.state.clicked });
+    for (let i = 0; i < DatosCircuitos.length; i++) {
+      <div>
+      Circuito ={DatosCircuitos[i].Circuito}
+      Pais={DatosCircuitos[i].Pais}
+      Longitud={DatosCircuitos[i].Longitud}
+      Tipo={DatosCircuitos[i].Tipo}
+      imagen={DatosCircuitos[i].imagen}
+      descripcion={DatosCircuitos[i].descripcion}
+      </div>
+    }
+    return lista;
+  }*/
 
   render() {
     return (
@@ -23,7 +38,7 @@ class Coches extends React.Component {
             <Col lg={8} md={6}>
               <Table responsive striped>
                 <thead>
-                  <tr >
+                  <tr>
                     <th>{GranPremios.id} </th>
                     <th>{GranPremios.field1}</th>
                     <th>{GranPremios.field2}</th>
@@ -31,33 +46,36 @@ class Coches extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                    {DatosCircuitos.map((item) => {
-                      return (
-                        <tr onClick={this.changeClicked.bind(this)}>
-                          <td>{item.Circuito}</td>
-                          <td>{item.Pais}</td>
-                          <td>{item.Longitud}</td>
-                          <td>{item.Tipo}</td>
-                        </tr>
-                      );
-                    })}
+                  {DatosCircuitos.map((item) => {
+                    return (
+                      <tr onClick={this.changeData.bind(this)}>
+                        <td>{item.Circuito}</td>
+                        <td>{item.Pais}</td>
+                        <td>{item.Longitud}</td>
+                        <td>{item.Tipo}</td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </Table>
-            </Col> 
+            </Col>
             <Col lg={4} md={6}>
               <Card style={{ width: '16rem' }}>
-                <Card.Img variant="top" src={DatosCircuitos[4].imagen} className={this.state.clicked} />
+                <Card.Img
+                  variant="top"
+                  src={DatosCircuitos[4].imagen}
+                  className={this.state.clicked}
+                />
                 <Card.Body>
                   <Card.Title>
                     {DatosCircuitos[4].Pais} - {DatosCircuitos[4].Circuito}
                   </Card.Title>
                   <Card.Text>
                     Longitud: {DatosCircuitos[4].Longitud}
-                    <p/>
+                    <p />
                     {DatosCircuitos[4].descripción}
                   </Card.Text>
                 </Card.Body>
-                      
               </Card>
             </Col>
           </Row>
