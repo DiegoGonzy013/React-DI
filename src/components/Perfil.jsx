0b7db666089429be86718e761903563c39056063
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-26 13:30:10 +0100

    1 file modified, 1 added

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import {Home} from'./Home';
+import  Home  from './Home';
+import { Card } from 'react-bootstrap';
+import { Usuarios } from '../data/Usuarios';
 
-export function Perfil() {
-  return (
+class Perfil extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Nombre: Usuarios[0].Nombre,
+      Email: Usuarios[0].Email,
+      Datospersonales: Usuarios[0].Datospersonales,
+      Avatar: Usuarios[0].Avatar,
+      Edad: Usuarios[0].Edad
+    };
+  }
+  return() {
     <div>
-
-</div>
-  );
+      <Card style={{ width: '16rem' }}>
+        <Card.Img variant="top" src={this.state.Avatar} />
+        <Card.Body>
+          <Card.Title>
+            Nombre: {this.state.Nombre}
+            Email: {this.state.Email} 
+            Edad: {this.state.Edad}
+          </Card.Title>
+          <Card.Text>
+            <p />
+            {this.state.Datospersonales}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Button variant="primary" type="button" onClick={this.limpiar}>
+        Limpiar
+      </Button>
+    </div>;
+  }
 }
+export default Perfil;
---
+++ src/data/Usuarios.js
@@ -0,0 +1,11 @@
+import Home from '../components/Home';
+
+export const Usuarios = [
+  {
+    Nombre: 'Diego',
+    Email: 'diego@gmail.com',
+    Datospersonales: 'Le gusta correr',
+    Avatar: 'aa',
+    Edad: '23 años',
+  },
+];
